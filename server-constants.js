cee18da05b77044a3394b19d83b2b4ea57ef554e
Rory <rc.oconnor2@gmail.com>
2022-04-21 10:33:58 -0400

    change scopes

--- server-constants.js
+++ server-constants.js
@@ -1,6 +1,6 @@
 module.exports = {
     // EXPLORER_SCOPES: "base_explorer item_preview item_download item_rename item_delete item_share item_upload root_readwrite annotation_view_all annotation_edit",
-    EXPLORER_SCOPES: "base_explorer item_preview item_download",
+    EXPLORER_SCOPES: "base_explorer item_preview item_download root_readwrite",
     RECENTS_SCOPES: "base_explorer item_preview item_download item_rename item_delete item_share item_upload root_readwrite annotation_view_all annotation_edit" ,
     PICKER_SCOPES: "base_picker item_preview item_share item_upload",
     UPLOADER_SCOPES: "base_upload",
